18d314f8a5daf2bd456655f140c63c6d8b5f0d81
lijonathan <jonnyli.inc@gmail.com>
2020-05-13 21:12:14 -0500

    recursive and iterative linkedlist reversal

---
+++ Add_Strings.java
@@ -0,0 +1,62 @@
+public class Add_Strings {
+
+/*
+Rating: Easy
+
+Given two non-negative integers num1 and num2
+represented as string, return the sum of num1 and num2.
+
+Note:
+
+    The length of both num1 and num2 is < 5100.
+    Both num1 and num2 contains only digits 0-9.
+    Both num1 and num2 does not contain any leading zero.
+    You must not use any built-in BigInteger library or
+    convert the inputs to integer directly.
+
+*/
+    public String addStrings(String num1, String num2) {
+    	//O(n) time, O(n) space where n = length of longest string
+    	StringBuffer sb = new StringBuffer();
+        int l1 = num1.length();
+        int l2 = num2.length();
+        int length = Math.max(l1, l2);
+        int i = 0;
+        boolean carry = false;
+        while (i < length)
+        {
+            int num1_val = 0;
+            int num2_val = 0;
+            if (l1 - i - 1 >= 0)
+            {
+                num1_val = Integer.parseInt(Character.toString(num1.charAt(l1 - i - 1)));
+            }
+            if (l2 - i - 1 >= 0)
+            {
+                num2_val = Integer.parseInt(Character.toString(num2.charAt(l2 - i - 1)));
+            }
+            int sum = 0;
+            if (carry)
+            {
+                sum = 1;
+            }
+            sum = sum + num1_val + num2_val;
+            if (sum > 9)
+            {
+                sum -= 10;
+                carry = true;
+            }
+            else
+            {
+                carry = false;
+            }
+            sb.append(Integer.toString(sum));
+            ++i;
+        }
+        if (carry)
+        {
+            sb.append("1");
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ Reverse_Linked_List.java
@@ -0,0 +1,63 @@
+public class Reverse_Linked_List {
+
+/*
+Rating: Easy
+
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+Follow up:
+
+A linked list can be reversed either
+iteratively or recursively.
+Could you implement both?
+
+
+*/
+	//O(n) time to traverse list, O(1) space excluding recursive call space
+	public class ListNode {
+	     int val;
+	     ListNode next;
+	     ListNode() {}
+	     ListNode(int val) { this.val = val; }
+	     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+
+    private void traversal(ListNode sentinel, ListNode head)
+    {
+    	// always move the next node as the next of
+    	// sentinel. Update current node's next to sentinel next
+    	// same as iterative, except traversal through list is
+    	// recursive
+        if(head != null)
+        {
+            ListNode iter = head.next;
+            head.next = sentinel.next;
+            sentinel.next = head;
+            traversal(sentinel, iter);
+        }
+    }
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode sentinel = new ListNode();
+        traversal(sentinel, head);
+        return sentinel.next;
+    }
+    public ListNode iterreverseList(ListNode head) {
+        ListNode sentinel = new ListNode();
+        ListNode iterator;
+        while(head != null)
+        {	// always move the next node as the next of
+        	// sentinel. Update current node's next to sentinel next
+            iterator = head.next;
+            head.next = sentinel.next;
+            sentinel.next = head;
+            head = iterator;
+        }
+        return sentinel.next;
+    }
+}
